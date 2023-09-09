63e4783f8c97d891be35865746e99a5865e0f233
Trevor Sharp <sharp.trevor@me.com>
2023-09-09 19:52:17 +0000

    Use node-rss-edge / Remove support for local file in enclosure

--- src/types/item-enclosure.ts
+++ src/types/item-enclosure.ts
@@ -2,10 +2,8 @@
 export interface ItemEnclosure {
   /** Url to the related file. */
   url: string;
-  /** Path to the related file on the filesystem. Used to fill out size and mime information. */
-  file?: string;
-  /** Number of bytes in the file. The length field will defualt to 0 if the `size` or `file` fields have not been set. */
+  /** Number of bytes in the file. The length field will defualt to 0 if the `size` field has not been set. */
   size?: number;
-  /** Mime type of the file. Will be guessed from the url if this parameter is not set. */
-  type?: string;
+  /** Mime type of the file. */
+  type: string;
 }
